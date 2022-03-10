6e9b7e0c1bc7c8cd1a621270fc9d5f1e0cdcef0f
Sandro <sandrogriebeler@gmail.com>
2022-03-10 17:31:23 -0300

    feat: index do integrador

---
+++ src/Integrator/index.ts
@@ -0,0 +1,52 @@
+export { DoocaClient, DoocaClientConfig } from '../Dooca'
+
+export {
+    AttributeCreateRequest,
+    AttributeCreateRequestParams,
+    AttributeUpdateRequest,
+    AttributeUpdateRequestParams
+} from '../Dooca/Requests/Attribute'
+
+export {
+    AttributeValueCreateRequest,
+    AttributeValueCreateRequestParams,
+    AttributeValueUpdateRequest,
+    AttributeValueUpdateRequestParams
+} from '../Dooca/Requests/AttributeValue'
+
+export {
+    BrandCreateRequest,
+    BrandCreateRequestParams,
+    BrandUpdateRequest,
+    BrandUpdateRequestParams
+} from '../Dooca/Requests/Brand'
+
+export {
+    CategoryCreateRequest,
+    CategoryCreateRequestParams,
+    CategoryUpdateRequest,
+    CategoryUpdateRequestParams
+} from '../Dooca/Requests/Category'
+
+export {
+    ColorCreateRequest,
+    ColorCreateRequestParams,
+    ColorUpdateRequest,
+    ColorUpdateRequestParams
+} from '../Dooca/Requests/Color'
+
+export {
+    ProductCreateRequest,
+    ProductCreateRequestParams,
+    ProductUpdateRequest,
+    ProductUpdateRequestParams
+} from '../Dooca/Requests/Product'
+
+export { StockMassUpdateRequest, StockMassUpdateRequestParams } from '../Dooca/Requests/Stock'
+
+export {
+    VariationCreateRequest,
+    VariationCreateRequestParams,
+    VariationUpdateRequest,
+    VariationUpdateRequestParams
+} from '../Dooca/Requests/Variation'
