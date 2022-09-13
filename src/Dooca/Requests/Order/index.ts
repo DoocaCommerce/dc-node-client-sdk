16645785bec062823a6f97281448b8ae6cd18f42
caio-datum <caio.rocha@datum.inf.br>
2022-09-13 20:43:00 -0300

    fix: importando classe

--- src/Dooca/Requests/Order/index.ts
+++ src/Dooca/Requests/Order/index.ts
@@ -2,4 +2,8 @@ export { OrderListRequest, OrderListRequestParams } from './OrderListRequest'
 export { OrderGetOneRequest, OrderGetOneRequestParams } from './OrderGetOneRequest'
 export { OrderCreateRequest, OrderCreateRequestParams } from './OrderCreateRequest'
 export { OrderUpdateRequest, OrderUpdateRequestParams } from './OrderUpdateRequest'
+export {
+    OrderStatusUpdateRequest,
+    OrderStatusUpdateRequestParams
+} from './OrderStatusUpdateRequest'
 export { OrderGetByTokenRequest, OrderGetByTokenRequestParams } from './OrderGetByTokenRequest'
